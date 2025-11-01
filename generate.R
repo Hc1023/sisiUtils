08395a929140fd3a810a396a47813336af985e2c
Hc1023 <sisih@zju.edu.cn>
2025-11-01 17:38:23 +0800

    Update generate.R

--- generate.R
+++ generate.R
@@ -16,8 +16,11 @@ system('git config --global user.name "Hc1023"')
 system('git config --global user.email "sisih@zju.edu.cn"')
 
 # 再次初始化 Git（如果之前已经初始化，可以跳过）
-usethis::use_git()
+usethis::use_git(message = "test")
 # 配置好本地 Git 后，再运行：
+usethis::use_github()
+# 如果已经有remote
+usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
 usethis::use_github()
 # 创建远程仓库（需要登录 GitHub 或者设置 PAT）
 # 推送本地仓库到 GitHub
