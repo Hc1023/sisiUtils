174b4a5a98871504ac6405f6f8b7ed1d2832064f
Hc1023 <sisih@zju.edu.cn>
2025-11-01 17:35:59 +0800

    test

--- generate.R
+++ generate.R
@@ -5,7 +5,9 @@ document()
 
 install()
 
+# 添加 README：
 usethis::use_readme_rmd()
+# 添加测试：
 usethis::use_testthat()
 
 # 设置你的名字
@@ -13,8 +15,18 @@ system('git config --global user.name "Hc1023"')
 # 设置你的邮箱
 system('git config --global user.email "sisih@zju.edu.cn"')
 
+# 再次初始化 Git（如果之前已经初始化，可以跳过）
+usethis::use_git()
+# 配置好本地 Git 后，再运行：
 usethis::use_github()
+# 创建远程仓库（需要登录 GitHub 或者设置 PAT）
+# 推送本地仓库到 GitHub
+
+
+# 打开你的 GitHub 仓库，确认文件已经上传。
+usethis::browse_github()
+
 devtools::install_github("Hc1023/sisiUtils")
 
 library(sisiUtils)
-?read_excel_allsheets
+
